7f82ca4950312ce181b6cd19224cc5759d9b3053
gaof <1425417918@qq.com>
2024-04-03 17:08:22 +0800

    集成jacypt实现数据加密功能

---
+++ src/main/java/com/example/accesslimitproject/controller/JasyptController.java
@@ -0,0 +1,25 @@
+package com.example.accesslimitproject.controller;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JasyptController {
+
+    @Autowired
+    private StringEncryptor stringEncryptor;
+
+    /**
+     * 加密
+     * @param content
+     * @return
+     */
+    @GetMapping("encrypt/{content}")
+    public String encrypt(@PathVariable String content) {
+        String encrypt = stringEncryptor.encrypt(content);
+        return encrypt;
+    }
+}
