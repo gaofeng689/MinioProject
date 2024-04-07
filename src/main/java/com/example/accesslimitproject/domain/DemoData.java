e74dec5ffebc1221ff5a463e0953c534b33e6ed7
gaof <1425417918@qq.com>
2024-04-07 15:16:50 +0800

    使用easyexcel自定义转换器，在@ExcelProperty里面加注解，同时实现一个转换器类，另外日期用string类型接收后用@DataTimeFormat注解处理，数字用String类型接收后用@NumberFormat注解处理。

--- src/main/java/com/example/accesslimitproject/domain/DemoData.java
+++ src/main/java/com/example/accesslimitproject/domain/DemoData.java
@@ -1,14 +1,16 @@
 package com.example.accesslimitproject.domain;
 
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.example.accesslimitproject.util.CustomStringStringConverter;
 import lombok.Data;
 
 @Data
 public class DemoData {
 
-//    @ExcelProperty(index = 2)
+    @ExcelProperty(index = 2,converter = CustomStringStringConverter.class)
     private String string;
-//    @ExcelProperty("字符串标题")
+    @ExcelProperty("字符串标题1")
     private String string1;
-//    @ExcelProperty("日期标题")
+    @ExcelProperty("字符串标题2")
     private String string2;
 }
