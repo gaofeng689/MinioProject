cbcdb3ef4c547d6de50c4dbe851dc6bd4dbd388a
gaof <1425417918@qq.com>
2024-04-07 14:50:11 +0800

    使用@ExcelProperty指定excel文件的列名解析

--- src/main/java/com/example/accesslimitproject/domain/DemoData.java
+++ src/main/java/com/example/accesslimitproject/domain/DemoData.java
@@ -1,11 +1,15 @@
 package com.example.accesslimitproject.domain;
 
+import com.alibaba.excel.annotation.ExcelProperty;
 import lombok.Data;
 
 @Data
 public class DemoData {
 
+    @ExcelProperty(index = 2)
     private String string;
+    @ExcelProperty("字符串标题")
     private String string1;
+    @ExcelProperty("日期标题")
     private String string2;
 }
