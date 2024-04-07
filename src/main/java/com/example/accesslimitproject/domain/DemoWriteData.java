22498174958217ed7789320f0a2459c20218dc59
gaof <1425417918@qq.com>
2024-04-07 17:34:56 +0800

    写入多个表头，通过@ExcelProperty的value属性设置

--- src/main/java/com/example/accesslimitproject/domain/DemoWriteData.java
+++ src/main/java/com/example/accesslimitproject/domain/DemoWriteData.java
@@ -7,13 +7,13 @@ import lombok.Data;
 @Data
 public class DemoWriteData {
 
-    @ExcelProperty(value ="字符串标题",index =0)
+    @ExcelProperty(value ={"主标题","字符串标题"},index =0)
     private String string;
 
-    @ExcelProperty(value ="日期标题",index =1)
+    @ExcelProperty(value ={"主标题","日期标题"},index =1)
     private String date;
 
-    @ExcelProperty(value ="数字标题",index =3)  //这里设置3会导致第二列为空，index不是必写的
+    @ExcelProperty(value ={"主标题","数字标题"},index =3)  //这里设置3会导致第二列为空，index不是必写的
     private Double doubleData;
 
     /**
